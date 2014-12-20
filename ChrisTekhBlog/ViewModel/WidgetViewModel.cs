3b52eabeb3503eca767168fd9b157a1fd4004576
christiannwamba <nwambachristian@gmail.com>
2014-12-20 00:12:39 -0800

    WVM

---
+++ ChrisTekhBlog/ViewModel/WidgetViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ChrisTekhBlog.Core;
+using ChrisTekhBlog.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChrisTekhBlog.ViewModel
+{
+    public class WidgetViewModel
+    {
+        public WidgetViewModel(IBlogRepository blogRepository)
+        {
+            Categories = blogRepository.Categories();
+            Tags = blogRepository.Tags();
+        }
+
+        public IList<Category> Categories { get; private set; }
+        public IList<Tag> Tags { get; private set; }
+    }
+}
